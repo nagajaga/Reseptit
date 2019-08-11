56ea45975b65751324bfae8f8a02ac6805115f29
Joel Wang <workworkjoel@gmail.com>
2019-08-12 02:10:22 +0300

    added login and account features

--- application/recipes/models.py
+++ application/recipes/models.py
@@ -11,6 +11,9 @@ class Recipe(db.Model):
     type = db.Column(db.String(144), nullable=False)
     description = db.Column(db.String(144), nullable=False)
 
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id'),
+                           nullable=False)
+                           
     def __init__(self, name):
         self.name = name
         self.type = "Regular"
--- application/recipes/views.py
+++ application/recipes/views.py
@@ -2,19 +2,23 @@ from application import app, db
 from flask import render_template, request, url_for, redirect
 from application.recipes.models import Recipe
 from application.recipes.forms import RecipeForm
+from flask_login import login_required, current_user
 
 
 @app.route("/recipes/new/")
+@login_required
 def recipes_form():
     return render_template("recipes/new.html", form=RecipeForm())
 
 
 @app.route("/recipes", methods=["GET"])
+@login_required
 def recipes_index():
     return render_template("recipes/list.html", recipes=Recipe.query.all())
 
 
 @app.route("/recipes/", methods=["POST"])
+@login_required
 def recipes_create():
     form = RecipeForm(request.form)
 
@@ -23,6 +27,7 @@ def recipes_create():
         
     t = Recipe(form.name.data)
     t.description = form.description.data
+    t.account_id = current_user.id
     
     db.session().add(t)
     db.session().commit()
