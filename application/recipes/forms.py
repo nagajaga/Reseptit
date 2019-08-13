4d2763409e86b3a9e449deb4e5af35622707ea42
Joel Wang <workworkjoel@gmail.com>
2019-08-13 21:19:43 +0300

    added bootstrap styling

--- application/recipes/forms.py
+++ application/recipes/forms.py
@@ -5,7 +5,7 @@ from wtforms import StringField, TextAreaField, validators
 class RecipeForm(FlaskForm):
     name = StringField("Recipe name", [validators.Length(min=2)])
     description = TextAreaField("Description", [validators.Length(max=144)])
-
+    content = TextAreaField("Content")
     class Meta:
         csrf = False
 
--- application/recipes/models.py
+++ application/recipes/models.py
@@ -1,20 +1,20 @@
 from application import db
 
+from application.models import Base
 
-class Recipe(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-                              onupdate=db.func.current_timestamp())
+
+class Recipe(Base):
 
     name = db.Column(db.String(144), nullable=False)
     type = db.Column(db.String(144), nullable=False)
     description = db.Column(db.String(144), nullable=False)
+    content = db.Column(db.String(4000), nullable=False)
 
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'),
                            nullable=False)
-                           
+
     def __init__(self, name):
         self.name = name
         self.type = "Regular"
         self.description = "none"
+        self.content = "none"
