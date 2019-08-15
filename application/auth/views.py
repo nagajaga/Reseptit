71dfbfcae96a3ddd4802c105ec1502282a754858
Joel Wang <workworkjoel@gmail.com>
2019-08-15 13:10:54 +0300

    added registration

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, RegistrationForm
 
 
 @app.route("/auth/login", methods=["GET", "POST"])
@@ -27,3 +27,23 @@ def auth_login():
 def auth_logout():
     logout_user()
     return redirect(url_for("index"))
+
+
+@app.route("/register/", methods=["POST"])
+def auth_register():
+    form = RegistrationForm(request.form)
+    if not form.validate():
+        return render_template("auth/registerform.html", form = form)
+    
+    t = User(form.name.data)
+    t.username = form.username.data
+    t.password = form.password.data
+
+    db.session().add(t)
+    db.session().commit()
+
+    return redirect(url_for("auth_login"))
+
+@app.route("/register/new/")
+def register_form():
+    return render_template("auth/registerform.html", form=RegistrationForm())
